7d83e0729c1932ead103abe9d8f2c27bd385e707
Ahana1105 <77888005+Ahana1105@users.noreply.github.com>
2022-08-13 10:30:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
-var img="";
-
+var img = "";
+var object_status = "";
 
 function setup() {
-canvas = createCanvas(640, 420);
-canvas.center();
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("model is loaded");
+
+    object_status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+    }
 }
 
 function preload() {
-img = loadImage('dog_cat.jpg');
+    img = loadImage('dog_cat.jpg');
 }
 
 function draw() {
-image(img, 0, 0, 640, 420);
-fill("black");
-text("Dog", 45, 75);
-noFill();
-stroke("#003333");
-rect(30,60,450,350);
+    image(img, 0, 0, 640, 420);
+
+    fill("black");
+    textSize(15);
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#003333");
+    rect(30, 60, 450, 350);
+
+    fill("black");
+    textSize(15);
+    text("Cat", 305, 85);
+    noFill();
+    stroke("#003333");
+    rect(300, 65, 300, 330);
 }
